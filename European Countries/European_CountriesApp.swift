770b425797ece83013037e71aee200f74eb25542
Hamed Majdi <hamed90_viva@yahoo.com>
2023-06-25 17:59:37 +0300

    Initial Commit

---
+++ European Countries/European_CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  European_CountriesApp.swift
+//  European Countries
+//
+//  Created by Hamed Majdi on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct European_CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
